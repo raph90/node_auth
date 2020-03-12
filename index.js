18579bbbec3bbbb658af49f058f7327aad970beb
Raphael Hetherington <raph37heth@gmail.com>
2020-03-12 09:24:59 +0000

    added env vars

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
 const express = require('express')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose');
 const http = require('http')
+
 const router = require('./router')
 const app = express()
 
@@ -11,7 +13,12 @@ app.use(morgan('combined'))
 app.use(bodyParser.json({ type: '*/*'}))
 router(app)
 
+
+// mongodb+srv://<username>:<password>@raphcluster-apijd.mongodb.net/test?retryWrites=true&w=majority
+
+
 //  server setup
+console.log(process.env.secret)
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app)
