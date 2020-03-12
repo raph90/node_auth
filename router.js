51a75eb55d2845aa8bc856687c0850303b1c7ddd
Raphael Hetherington <raph37heth@gmail.com>
2020-03-12 09:59:01 +0000

    added controllers and signup route

--- router.js
+++ router.js
@@ -1,7 +1,5 @@
-
+const Authentication = require('./controllers/authentication')
 
 module.exports = app => {
-    app.get('/', (req, res, next) => {
-        res.send("hello there")
-    })
+   app.post('/signup', Authentication.signup)
 }
