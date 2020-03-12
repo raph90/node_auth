9dcb4da9703bf711819c1dcf2ffd0466fb318071
Raphael Hetherington <raph37heth@gmail.com>
2020-03-12 09:15:13 +0000

    added user schema

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const mongoose = require('mongoose');
 const http = require('http')
-
+const router = require('./router')
+const app = express()
 
 // app setup 
-
+app.use(morgan('combined'))
+app.use(bodyParser.json({ type: '*/*'}))
+router(app)
 
 //  server setup
+
+const port = process.env.PORT || 3000;
+const server = http.createServer(app)
+
+server.listen(port)
+console.log("server listening on ", port)
